a2b4c38be5f0c93854367e9bf9028a0b1476ca27
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-03-11 01:18:07 +0530

    add plugin main file

---
+++ instamojo-for-give.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* Instamojo for Give - WordPress Plugin
+*
+* @package           Instamojo for Give
+* @author            Mehul Gohil
+* @copyright         2021 Mehul Gohil <hello@mehulgohil.com>
+* @license           GPL-2.0-or-later
+*
+* @wordpress-plugin
+*
+* Plugin Name:       Instamojo for Give
+* Plugin URI:        https://wordpress.org/plugins/instamojo-for-give
+* Description:       Accept donations via Instamojo payment gateway using GiveWP.
+* Version:           1.0.0
+* Requires at least: 4.8
+* Requires PHP:      5.6
+* Author:            Mehul Gohil
+* Author URI:        https://mehulgohil.com
+* Text Domain:       instamojo-for-give
+* License:           GPL v2 or later
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+namespace Instamojo\GiveWP;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load Constants.
+require_once __DIR__ . '/config/constants.php';
+
+// Automatically loads files used throughout the plugin.
+require_once INSTAMOJO_FOR_GIVE_PLUGIN_DIR . 'vendor/autoload.php';
+
+// Initialize the plugin.
+$plugin = new Plugin();
+$plugin->register();
