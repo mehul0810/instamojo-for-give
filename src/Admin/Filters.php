40baf363c69b7ef76f58416fa06f9fc63213540a
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-03-11 01:30:48 +0530

    add settings and support link to plugin listing

--- src/Admin/Filters.php
+++ src/Admin/Filters.php
@@ -22,6 +22,32 @@ class Filters {
      * @access public
      */
     public function __construct() {
-        
+        add_filter( 'plugin_action_links_' . INSTAMOJO_FOR_GIVE_PLUGIN_BASENAME, [ $this, 'add_plugin_action_links' ] );
     }
+
+    /**
+	 * Plugin page action links.
+	 *
+	 * @param array $actions An array of plugin action links.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array
+	 */
+	public function add_plugin_action_links( $actions ) {
+		$new_actions = [
+			'settings' => sprintf(
+				'<a href="%1$s">%2$s</a>',
+				admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=instamojo' ),
+				esc_html__( 'Settings', 'instamojo-for-give' )
+			),
+			'support'  => sprintf(
+				'<a target="_blank" href="%1$s">%2$s</a>',
+				esc_url_raw( 'https://wordpress.org/support/plugin/instamojo-for-give/' ),
+				esc_html__( 'Support', 'instamojo-for-give' )
+			),
+		];
+
+		return array_merge( $new_actions, $actions );
+	}
 }
