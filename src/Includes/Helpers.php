75dd29c3e40a82fc07d8c47a2b0472a85cf8afd7
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-03-12 00:06:08 +0530

    add helper fn get api endpoint

--- src/Includes/Helpers.php
+++ src/Includes/Helpers.php
@@ -15,4 +15,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Helpers {
+    /**
+     * Get API Endpoint.
+     * 
+     * @since  1.0.0
+     * @access public
+     *
+     * @return void
+     */
+    public static function get_api_endpoint() {
+        $version = apply_filters( 'instamojo_for_give_api_version' , '1.1' );
+        $url     = "https://instamojo.com/api/{$version}/";
+
+        if ( give_is_test_mode() ) {
+            $url = "https://test.instamojo.com/api/{$version}/";
+        }
+        
+        return $url;
+    }
 }
