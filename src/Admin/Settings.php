cf4239d5501cbec2ab80c02fa9f5f9c63a72f741
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-04-19 01:33:09 +0530

    add notice to the settings page for help

--- src/Admin/Settings.php
+++ src/Admin/Settings.php
@@ -26,8 +26,45 @@ class Settings {
 	 * @return void
 	 */
 	public function __construct() {
+		add_action( 'give_admin_field_cta_notice', [ $this, 'render_cta_notice' ], 10, 2 );
 		add_filter( 'give_get_sections_gateways', [ $this, 'register_sections' ] );
 		add_filter( 'give_get_settings_gateways', [ $this, 'register_settings' ] );
+	}
+
+	/**
+	 * Render CTA Notice.
+	 *
+	 * @param array  $value        List of settings parameters.
+	 * @param string $option_value Option value.
+	 * 
+	 * @since  1.0.0
+	 * @access public
+	 * 
+	 * @return mixed
+	 */
+	public function render_cta_notice( $value, $option_value ) {
+		$wrapper_class = ! empty( $value['wrapper_class'] ) ? 'class="' . $value['wrapper_class'] . '"' : '';
+		?>
+		<tr valign="top" <?php echo esc_html( $wrapper_class ); ?>>
+			<td class="give-cta-notice-wrap" style="padding: 0;" colspan="2">
+				<div class="give-cta-notice" style="background-color: #fff; margin-bottom: 20px;">
+					<h3 style="text-transform: uppercase; font-size: 14px; font-weight: 700; margin-top: 0; border-bottom: 2px solid #f5f5f5; padding: 12px 20px;">
+						<?php echo $value['title']; ?>
+					</h3>
+					<ol style="margin: 10px 35px; font-size: 13px">
+						<?php
+						foreach ( $value['steps'] as $step ) {
+							echo $step;
+						}
+						?>
+					</ol>
+					<p class="give-field-description">
+						<?php echo $value['desc']; ?>
+					</p>
+				</div>
+			</td>
+		</tr>
+		<?php
 	}
 
 	/**
@@ -65,6 +102,35 @@ class Settings {
 					[
 						'type' => 'title',
 						'id'   => 'give_title_gateway_settings_instamojo_start',
+					],
+					[
+						'type'  => 'cta_notice',
+						'id'    => 'give_title_gateway_settings_instamojo_start',
+						'name'  => esc_html__( 'Need Help?', 'instamojo-for-give' ),
+						'desc'  => '',
+						'steps' => [
+							sprintf(
+								'<li><strong>%1$s</strong> <a href="%2$s">%3$s</a> %4$s</li>',
+								esc_html__( 'Don\'t have an Instamojo Account?', 'instamojo-for-give' ),
+								esc_url_raw( 'https://mehulgohil.com/recommends/instamojo' ),
+								esc_html__( 'Click here', 'instamojo-for-give' ),
+								esc_html__( 'to create one.', 'instamojo-for-give' )
+							),
+							sprintf(
+								'<li><strong>%1$s</strong> <a href="%2$s">%3$s</a> %4$s</li>',
+								esc_html__( 'Existing Instamojo Customer?', 'instamojo-for-give' ),
+								esc_url_raw( 'mailto:hello@mehulgohil.com' ),
+								esc_html__( 'Email me', 'instamojo-for-give' ),
+								esc_html__( 'the email address of your Instmojo account for additional Instamojo account related help.', 'instamojo-for-give' )
+							),
+							sprintf(
+								'<li><strong>%1$s</strong> <a href="%2$s">%3$s</a> %4$s</li>',
+								esc_html__( 'Facing an issue with the plugin?', 'instamojo-for-give' ),
+								esc_url_raw( 'https://wordpress.org/support/plugin/mg-instamojo-for-give/' ),
+								esc_html__( 'Click here', 'instamojo-for-give' ),
+								esc_html__( 'to create a ticket on WordPress support forums.', 'instamojo-for-give' )
+							),
+						],
 					],
 					[
 						'name'    => esc_html__( 'Live - Private API Key', 'instamojo-for-give' ),
